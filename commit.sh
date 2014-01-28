9b6ab99b0582a464f8813cdaba3e6b455b503a18
Xavier Grand <grand.xavier@gmail.com>
2014-01-28 00:21:15 -0800

    Added features on commit.sh

--- commit.sh
+++ commit.sh
@@ -1,7 +1,35 @@
 #! /bin/sh
 
-echo "Generate README.mds"
+cd `dirname $0`
 
+if [ "$#" = "1" -a "$1" = "--revert" ] ; then
+  echo "RESET DU DERNIER COMMIT DES DEPOTS"
+  sleep 10
+  for dir in ../algoliasearch-client-*; do
+    cd $dir
+    echo "$dir"
+    git reset ^HEAD
+  done
+  exit 0
+fi
+
+if [ "$#" = "1" -a "$1" = "--show" ] ; then
+  echo "Generate README.mds"
+  ./doc-generator.rb config.json
+  for dir in ../algoliasearch-client-*; do
+    cd $dir
+    echo "$dir"
+    git diff README.md
+  done
+  exit 0
+fi
+
+if [ "$#" != "1" -o "$1" != "--push" ] ; then
+  echo "Usage: ./commit {--show|--revert|--push}"
+  exit 1
+fi
+
+echo "Generate README.mds"
 ./doc-generator.rb config.json
 if [ "$?" != "0" ] ; then
   echo "Generation failed: stop"
@@ -26,3 +54,4 @@ for dir in ../algoliasearch-client-*; do
 done
 
 echo "README.mds commited !"
+exit 0
